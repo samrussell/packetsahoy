20aea4e6549d843c0d0703d7dd38266b0e8ffc9b
Sam Russell <sam.h.russell@gmail.com>
2015-09-27 23:07:15 +1300

    started on tests, all of them fail

--- lib/match.rb
+++ lib/match.rb
@@ -1,2 +1,18 @@
+require 'netaddr'
+
 class Match
+  def source_ip
+  end
+
+  def destination_ip
+  end
+
+  def source_port
+  end
+
+  def destination_port
+  end
+
+  def protocol
+  end
 end
--- spec/match_spec.rb
+++ spec/match_spec.rb
@@ -3,4 +3,35 @@ require "match"
 # A Match describes a combination of IP/protocol/port
 # that is used to filter network traffic
 describe Match do
+  let(:match) { Match.new }
+
+  describe '#source_ip' do
+    subject(:source_ip) { match.source_ip }
+
+    it { is_expected.to be_a_kind_of(NetAddr::CIDR) }
+  end
+
+  describe '#destination_ip' do
+    subject(:destination_ip) { match.destination_ip }
+
+    it { is_expected.to be_a_kind_of(NetAddr::CIDR) }
+  end
+
+  describe '#source_port' do
+    subject(:source_port) { match.source_port }
+    
+    it { is_expected.to_not be nil }
+  end
+
+  describe '#destination_port' do
+    subject(:destination_port) { match.destination_port }
+
+    it { is_expected.to_not be nil }
+  end
+
+  describe '#protocol' do
+    subject(:protocol) { match.protocol }
+
+    it { is_expected.to_not be nil }
+  end
 end
